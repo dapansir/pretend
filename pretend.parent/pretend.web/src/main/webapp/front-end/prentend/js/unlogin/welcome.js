7b66cfd9d836538f71991208720b41b80439e989
dapansir <289032576@qq.com>
2019-08-21 21:24:39 +0800

    增加左侧菜单动画效果

--- pretend.parent/pretend.web/src/main/webapp/front-end/prentend/js/unlogin/welcome.js
+++ pretend.parent/pretend.web/src/main/webapp/front-end/prentend/js/unlogin/welcome.js
@@ -1,4 +1,39 @@
-function ch(){
-	
-	
+function hideUl(ele){
+	$(ele).next().toggle(1000);
+    changeGlyphiconMenu(ele);
 }
+
+function changeGlyphiconMenu(ele){
+    var temp = $(ele).find(".glyphicon-menu-down");
+    if(temp.length == 0){
+        removeLeft(ele);
+    }else{
+        removeDown(ele);
+    }
+
+}
+
+function removeLeft(ele){
+    $(ele).find(".glyphicon-menu-left").addClass("glyphicon-menu-down");
+    $(ele).find(".glyphicon-menu-left").removeClass("glyphicon-menu-left");
+}
+
+function removeDown(ele){
+    $(ele).find(".glyphicon-menu-down").addClass("glyphicon-menu-left");
+    $(ele).find(".glyphicon-menu-down").removeClass("glyphicon-menu-down");
+}
+
+$(document).ready(function () {
+    $("aside").find("[name='treenodecontrol']").on("click",function () {
+        hideUl(this);
+    });
+    $("aside").find("ul.nav-sidebar").find("li").on("click",function () {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+        $(this).siblings().each(function (index,item) {
+            removeDown(item);
+            $(item).find("[name='treenodecontrol']").next().hide(1000);
+        });
+    });
+   
+});
